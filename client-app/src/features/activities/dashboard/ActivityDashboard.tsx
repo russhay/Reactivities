7a3e3da35673ebabc4a25b354bf27e2674c1c326
Russell Schoech <rschoech@icloud.com>
2020-05-13 15:44:14 -0500

    Almost done with Section 5 of course

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,41 @@
+﻿import React from 'react'
+import { Grid, List } from 'semantic-ui-react'
+import { IActivity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+
+interface IProps {
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
+    editMode: boolean;
+    setEditMode: (editMode: boolean) => void;
+    setSelectedActivity: (activity: IActivity | null) => void;
+}
+
+const ActivityDashboard: React.FC<IProps> = ({
+    activities,
+    selectActivity,
+    selectedActivity,
+    editMode,
+    setEditMode,
+    setSelectedActivity
+}) => {
+    return (
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityList activities={activities} selectActivity={selectActivity}/>
+            </Grid.Column>
+            <Grid.Column width={6}>
+                {selectedActivity && !editMode && (
+                    <ActivityDetails activity={selectedActivity} setEditMode={setEditMode}
+                        setSelectedActivity={setSelectedActivity}/>
+                )}
+                {editMode && <ActivityForm activity={selectedActivity!} setEditMode={setEditMode} />}
+            </Grid.Column>
+        </Grid>
+    );
+};
+
+export default ActivityDashboard;
