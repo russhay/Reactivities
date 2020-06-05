5352c3aa5e70e300eb0520556f37d02c84d213df
Russell Schoech <rschoech@icloud.com>
2020-06-05 14:49:56 -0500

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,10 +2,11 @@
 import { Item, Button, Label, Segment } from 'semantic-ui-react'
 import {observer} from "mobx-react-lite";
 import ActivityStore from '../../../app/stores/activityStore'
+import {Link} from "react-router-dom";
 
 const ActivityList: React.FC = () => {
     const activityStore = useContext(ActivityStore);
-    const {activitiesByDate, selectActivity, deleteActivity, submitting, target} = activityStore;
+    const {activitiesByDate, deleteActivity, submitting, target} = activityStore;
     return (
         <Segment clearing>
             <Item.Group divided>
@@ -19,7 +20,7 @@ const ActivityList: React.FC = () => {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue' />
                                 <Button 
                                     name={activity.id}
                                     loading={target === activity.id && submitting} 
